a2ef6215850ac5798f6b9125112200e07592c88a
Scott Yelvington <scottyelvington@gmail.com>
2018-06-29 09:36:10 -0700

    Reformatted code to spaces from tabs for readability.

--- Tree Compare/main.m
+++ Tree Compare/main.m
@@ -10,188 +10,188 @@
 
 struct treeNode {
 	
-	int 				value;
-	int 				count;
+     int                 value;
+     int                 count;
 	
-	struct treeNode 	**children;
+     struct treeNode     **children;
 };
 
 struct listElement {
 	
-	//index, aPointer, bPointer
-	NSUInteger		index;
+     //index, aPointer, bPointer
+     NSUInteger          index;
 	
-	struct treeNode	*treeA,
-					*treeB;
+     struct treeNode     *treeA,
+                         *treeB;
 	
-	struct listElement	*previous,
-					*next;
+     struct listElement  *previous,
+                         *next;
 };
 
 struct linkedList {
 	
-	NSInteger			length;
+     NSInteger           length;
 	
-	//previous struct, last struct
-	struct listElement	*last;
+     //previous struct, last struct
+     struct listElement  *last;
 };
 
 
 
 bool areTreesIdentical(struct treeNode *candidateTreeA, struct treeNode *candidateTreeB) {
 	
-	struct treeNode	*treeA 		= candidateTreeA,
-					*treeB 		= candidateTreeB;
+     struct treeNode     *treeA         = candidateTreeA,
+                         *treeB         = candidateTreeB;
 	
-	struct linkedList	list;
+     struct linkedList   list;
 	
-	struct listElement	*curElement,
-					*newElement;
+     struct listElement  *curElement,
+                         *newElement;
 	
-	NSInteger			depth		= 0,
-					index		= 0;
+     NSInteger           depth          = 0,
+                         index          = 0;
 	
-	int 				elementSize	= sizeof(struct listElement);
+     int                 elementSize    = sizeof(struct listElement);
 	
-	bool				match		= true;
+     bool                match          = true;
 	
-	//add first list container
-	curElement					= malloc(elementSize);
-	curElement->previous			= NULL;
-	
-	
-	//initialize the list struct
-	list.last						= curElement;
-	list.length					= 1;
+     //add first list container
+     curElement                         = malloc(elementSize);
+     curElement->previous               = NULL;
 	
 	
-	do {
-		//test if the current tree's values and counts are the same,
-		//if they're not, the trees are dissimilar and we don't want to proceed
-		if (treeA->value != treeB->value 	||
-		    treeA->count != treeB->count	) {
+     //initialize the list struct
+     list.last                          = curElement;
+     list.length                        = 1;
+	
+	
+     do {
+          //test if the current tree's values and counts are the same,
+          //if they're not, the trees are dissimilar and we don't want to proceed
+          if (treeA->value != treeB->value      ||
+              treeA->count != treeB->count     ) {
 			
-			match				= false;
+               match                    = false;
 			
-			break;
-		}
+               break;
+          }
 		
-		//next, check if the current tree has more children to itterate through,
-		//if so, we want to go downwards; if not, we want to go upwards
-		if (treeA->count && index < treeA->count) {
+          //next, check if the current tree has more children to itterate through,
+          //if so, we want to go downwards; if not, we want to go upwards
+          if (treeA->count && index < treeA->count) {
 			
-			//save our progress at this depth
-			curElement->index		= index;
-			curElement->treeA		= treeA;
-			curElement->treeB		= treeB;
+               //save our progress at this depth
+               curElement->index        = index;
+               curElement->treeA        = treeA;
+               curElement->treeB        = treeB;
 			
-			treeA				= treeA->children[index];
-			treeB				= treeB->children[index];
+               treeA                    = treeA->children[index];
+               treeB                    = treeB->children[index];
 			
-			depth++;
+               depth++;
 			
-			//check if we need to add a new element to list or not
-			if (list.length == depth) {
+               //check if we need to add a new element to list or not
+               if (list.length == depth) {
 				
-				newElement		= malloc(elementSize);
-				newElement->previous= curElement;
+                    newElement          = malloc(elementSize);
+                    newElement->previous= curElement;
 				
-				curElement->next	= newElement;
-				curElement		= newElement;
+                    curElement->next    = newElement;
+                    curElement          = newElement;
 				
-				//modify the list struct
-				list.last			= newElement;
-				list.length		+= 1;
-			} else {
+                    //modify the list struct
+                    list.last           = newElement;
+                    list.length         += 1;
+               } else {
 				
-				//get next element
-				newElement		= curElement->next;
-				curElement		= newElement;
-			}
+                    //get next element
+                    newElement          = curElement->next;
+                    curElement          = newElement;
+               }
 			
-			continue;
-		}
+               continue;
+          }
 		
-		depth--;
+          depth--;
 		
-		if (depth >= 0) {
-			curElement 			= curElement->previous;
-			treeA				= curElement->treeA;
-			treeB				= curElement->treeB;
-			index				= curElement->index+1;
-		}
+          if (depth >= 0) {
+               curElement               = curElement->previous;
+               treeA                    = curElement->treeA;
+               treeB                    = curElement->treeB;
+               index                    = curElement->index+1;
+          }
 		
-	} while (depth >= 0);
+     } while (depth >= 0);
 	
-	newElement					= list.last;
+     newElement                         = list.last;
 	
-	//add the free memory step here
-	for (NSInteger i = list.length; i>0; i--) {
+     //add the free memory step here
+     for (NSInteger i = list.length; i>0; i--) {
 		
-		curElement				= newElement->previous;
+          curElement                    = newElement->previous;
 		
-		free(newElement);
+          free(newElement);
 		
-		newElement				= curElement;
-	}
+          newElement                    = curElement;
+     }
 	
-	return match;
+     return match;
 }
 
 struct treeNode* generateRandomTree (int maxChildren, int maxDepth) {
 	
-	struct treeNode	*node		= malloc(sizeof(struct treeNode));
+     struct treeNode     *node          = malloc(sizeof(struct treeNode));
 	
-	node->value					= arc4random();
-	node->count					= (maxDepth > 0) ? arc4random()%maxChildren : 0;
+     node->value                        = arc4random();
+     node->count                        = (maxDepth > 0) ? arc4random()%maxChildren : 0;
 	
-	if (node->count) {
+     if (node->count) {
 		
-		node->children				= malloc(node->count * sizeof(struct treeNode *));
+          node->children                = malloc(node->count * sizeof(struct treeNode *));
 		
-		for (int i = 0; i < node->count; i++) {
+          for (int i = 0; i < node->count; i++) {
 			
-			node->children[i]		= generateRandomTree(maxChildren, maxDepth-1);
-		}
-	}
+               node->children[i]        = generateRandomTree(maxChildren, maxDepth-1);
+          }
+     }
 	
-	return node;
+     return node;
 }
 
 void destroyRandomTree (struct treeNode *node) {
 	
-	if (node->count) {
+     if (node->count) {
 		
-		for (int i = 0; i < node->count; i++) {
+          for (int i = 0; i < node->count; i++) {
 			
-			destroyRandomTree(node->children[i]);
-		}
+               destroyRandomTree(node->children[i]);
+          }
 		
-		free(node->children);
-	}
+          free(node->children);
+     }
 	
-	free(node);
+     free(node);
 }
 
 
 int main(int argc, const char * argv[]) {
 	
-	//create randomized trees
-	struct treeNode	*nodeA		= generateRandomTree(5, 5),
-					*nodeB		= generateRandomTree(5, 5);
+     //create randomized trees
+     struct treeNode     *nodeA         = generateRandomTree(5, 5),
+                         *nodeB         = generateRandomTree(5, 5);
 	
-	//test the trees against eachother to produce a fail, and against themselves to produce a true
-	bool 			firstResult	= areTreesIdentical(nodeA, nodeA),//same
-					secondResult	= areTreesIdentical(nodeB, nodeB),//same
-					thirdResult	= areTreesIdentical(nodeA, nodeB);//not same
+     //test the trees against eachother to produce a fail, and against themselves to produce a true
+     bool                firstResult    = areTreesIdentical(nodeA, nodeA),//same
+                         secondResult   = areTreesIdentical(nodeB, nodeB),//same
+                         thirdResult    = areTreesIdentical(nodeA, nodeB);//not same
 	
-	NSLog(firstResult ? @"The trees are the same" : @"The trees are not the same");
-	NSLog(secondResult ? @"The trees are the same" : @"The trees are not the same");
-	NSLog(thirdResult ? @"The trees are the same" : @"The trees are not the same");
+     NSLog(firstResult ? @"The trees are the same" : @"The trees are not the same");
+     NSLog(secondResult ? @"The trees are the same" : @"The trees are not the same");
+     NSLog(thirdResult ? @"The trees are the same" : @"The trees are not the same");
 	
-	//remove trees from memory
-	destroyRandomTree(nodeA);
-	destroyRandomTree(nodeB);
-		
-	return 0;
+     //remove trees from memory
+     destroyRandomTree(nodeA);
+     destroyRandomTree(nodeB);
+	
+     return 0;
 }
